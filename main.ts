97561fd719f5915fb09ccbe974a955fa10d312a9
Lluis Maria Perpiñà <lperpina@xtec.cat>
2024-06-21 12:12:44 +0200

    Afegida la pedra

--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ input.onGesture(Gesture.Shake, function () {
     ma = randint(1, 3)
     if (ma == 1) {
         basic.showIcon(IconNames.Square)
+    } else if (ma == 2) {
+        basic.showIcon(IconNames.SmallSquare)
     } else {
         basic.showIcon(IconNames.Scissors)
     }
